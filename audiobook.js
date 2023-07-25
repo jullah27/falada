571fb884a10731c89282c0fbb607a378dc21cc5d
jullah27 <121064010+jullah27@users.noreply.github.com>
2023-07-25 17:59:04 +0200

    Update audiobook.js

--- audiobook.js
+++ audiobook.js
@@ -3,24 +3,35 @@ let wHeight = window.innerHeight;
 let sliderA;
 let sliderB;
 let sliderC;
-let language;
+let languageButton;
+let language = spanisch;
 let audioESP;
 let audioGER;
 let playButton;
 
+
 function preload(){
-   soundFormats('mp3');
-    audioESP = loadSound('assets/text_1')
+    soundFormats('mp3');
+    audioESP = loadSound('assets/text_1');
+    audioGER = loadSound('assets/text_2');
 }
 
 function setup(){
     createCanvas(window.innerWidth, window.innerHeight);
+    // create slider for music
     sliderA = createSlider(0,100,0);
     sliderA.position(20,20);
     sliderA.style('width', '400px');
+
+    //play Button for all
     playButton = createButton('Play');
     playButton.position(wWidth/2, wHeight/2);
     playButton.mousePressed(playFunction);
+
+    //language Button for switching languages
+    languageButton = createButton('español')
+    languageButton.position(wWidth/2, wHeight/2-20);
+    languageButton.mousePressed(languageFunction);
 }
 
 function draw(){
@@ -32,10 +43,26 @@ function draw(){
 function playFunction(){
     if (audioESP.isPlaying()){
         audioESP.stop();
+        audioGER.stop();
         playButton.html('Play');
     }    
     else{
         audioESP.play();
+        audioGER.play();
         playButton.html('Stop');
+    }
+}
+
+function language(){
+    if (language = spanisch){
+        language = deutsch;
+        audioESP.setVolume(0);
+        audioGER.setVolume(0.8);
+    }
+    else{
+        langauge = spanisch;
+        audioESP.setVolume(0.8);
+        audioGER.setVolume(0);
+
     }
 }
